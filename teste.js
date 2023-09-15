05499c0baf833a01d63f32cfd8f475c6dbd9b163
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:47:13 -0300

    ultimo commit da aula de hoje

---
+++ teste.js
@@ -0,0 +1,16 @@
+//Criando um Alert em JS
+alert("AAAAAAAAA");
+
+/*
+Outro jeito de fazer comentarios
+*/
+
+//Forma antiga de se adicionar uma mensagem na tela
+document.write('NAO AGUENTO MAIS OLA MUNDO');
+
+//Forma de colocar mensagem no console
+console.log('Teste mensagem console');
+
+//Criando uma variavel
+let mensagem = "Oi";
+console.log("Mensagem de variavel:", mensagem);
