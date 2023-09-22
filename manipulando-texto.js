341520b3e9ef374c1833174d33f455d451c66b6c
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:51:18 -0300

    Adicionando arquivo de manipulação de texto e tipos de dados

---
+++ manipulando-texto.js
@@ -0,0 +1,19 @@
+let numFloat = 123.543;
+//console.log(typeof numFloat);
+
+// toString() é uma função nativa, ou seja, foi criada no javaScript, para converter para string
+// String() é nativa tambem, serve para converter para string, é mais nova.
+
+//console.log(typeof numFloat.toString());
+//console.log(typeof String());
+
+// Isso daqui é um Array, é uma lista com varias variaveis
+let linguagens = ["Java", "C#", "Python"];
+//                   0      1       2                     
+//console.log(linguagens[0]);
+
+let txt = "Texto teste";
+
+console.log(txt.replace("teste", "testado"));
+
+//.replace é outra função nativa. Substitui o primeiro valor (teste) e trocou pelo segundo valor (testado)
---
+++ tipos-dados.js
@@ -0,0 +1,18 @@
+// Numeros inteiros
+let idadeAluno = 18;
+
+// Numeros flutuantes
+let valorProduto = 5.35;
+
+// String
+let nomeAluno = "Pedro Henrique";
+
+// Boolean
+let casado = false;
+
+// Array
+
+//console.log(typeof idadeAluno);
+//console.log(typeof valorProduto);
+//console.log(typeof nomeAluno);
+//console.log(typeof casado);
