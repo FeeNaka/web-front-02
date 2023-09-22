eda15070ce32b09c87d2b8890d8da08aaf320c82
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:08:13 -0300

    Adicionando arquivo de variavel

---
+++ variavel.js
@@ -0,0 +1,10 @@
+// Cria uma variavel de valor indefinido
+let nome;
+
+// Cria uma variavel vazia
+let disciplina = null;
+
+// Cria uma variavel com valor
+let texto = "Exemplo";
+
+console.log(texto);
