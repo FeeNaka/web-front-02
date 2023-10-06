f753829cbbbffbc5aedfbe0e40d44ab60f7174e8
Felipe <113949309+FelipeNakama@users.noreply.github.com>
2023-10-06 11:46:25 -0300

    Add files via upload

---
+++ aula006/switch.js
@@ -0,0 +1,13 @@
+let teste = 3
+
+switch (teste){
+    case 1:
+        console.log("Foi de primeira")
+        break
+    case 2:
+        console.log("Foi na segunda")
+        break
+    default:
+        console.log("Nao foi nem na primeira nem na segunda")
+}
+
---
+++ aula006/valor.js
@@ -0,0 +1,7 @@
+let valor = 5;
+
+if(valor > 7) {
+    console.log('O valor é maior que 7');
+} else {
+    console.log('O valor é outro')
+}
